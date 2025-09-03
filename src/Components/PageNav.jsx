164748b901e837bf6c72a302b45f7a81aea24f01
KingAkande <kingakande@gmail.com>
2025-09-03 10:08:55 +0100

    created a general pattern for buttons

--- src/Components/PageNav.jsx
+++ src/Components/PageNav.jsx
@@ -4,30 +4,49 @@ import Header from "../Pages/Header";
 
 const PageNav = () => {
   return (
-    <nav className="inline-flex gap-6 bg-red-500">
-      <div>
-        {/* <NavLink to="/header">Header</NavLink> */}
-      </div>
-      <div>
+    <nav className="flex flex-wrap  mx-auto justify-center text-sm  font-medium w-[320px] gap-[14px] p-2 bg">
+      {/* <div> 
+        <NavLink to="/header">Header</NavLink>
+      </div> */}
+      {/* <div>
         <NavLink to="/login">Login</NavLink>
+      </div> */}
+      <div>
+        <NavLink to="/dashboard">
+          <Button name="Dashboard" emoji="🏠" />
+        </NavLink>
       </div>
       <div>
-        <NavLink to="/dashboard">Dashboard</NavLink>
+        <NavLink to="/transaction">
+          <Button name="Transaction" emoji="💳" />
+        </NavLink>
       </div>
       <div>
-        <NavLink to="/transaction">Transaction</NavLink>
+        <NavLink to="/report">
+          <Button name="Report" emoji="📊" />
+        </NavLink>
       </div>
       <div>
-        <NavLink to="/report">Report</NavLink>
+        <NavLink to="/budget">
+          <Button name="Budget" emoji="🎯" />
+        </NavLink>
       </div>
       <div>
-        <NavLink to="/budget">Budget</NavLink>
-      </div>
-      <div>
-        <NavLink to="/settings">Setting</NavLink>
+        <NavLink to="/settings">
+          <Button name="Setting" emoji="⚙️" />
+        </NavLink>
       </div>
     </nav>
   );
 };
 
 export default PageNav;
+
+function Button({ emoji, name }) {
+  return (
+    <button className="border-2 border-[#e2e8f0] cursor-pointer hover:text-[#4f46e5] transition-colors duration-600 ease-in-out   hover:border-[#4f46e5] rounded-2xl  px-2 py-1">
+      {emoji}
+      <span className="pl-1">{name}</span>
+    </button>
+  );
+}
--- src/Pages/Header.jsx
+++ src/Pages/Header.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 const Header = () => {
   return (
-    <div className="w-[320px] flex items-center justify-between">
-      <h1>💰 FinanceTracker</h1>
+    <div className="w-[320px] flex text-white mx-auto items-center justify-between bg-[#4f46e5] p-6">
+      <h1 className="font-bold ">💰 FinanceTracker</h1>
       <div className="flex gap-4 items-baseline ">
-        <h1>Zain Abu</h1>
+        <h1 className="">Zain Abu</h1>
         <button className="rounded-full p-2 bg-amber-400">ZD</button>
       </div>
     </div>
@@ -13,3 +13,6 @@ const Header = () => {
 };
 
 export default Header;
+
+
+//md:w-[690px] lg:w-[768px]
